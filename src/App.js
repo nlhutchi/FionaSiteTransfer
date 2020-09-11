8b52ce94dbb3247a6340e124c55f3a72ef3b274d
Nathan <hutchinsonmchone@gmail.com>
2020-09-10 22:25:40 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import AppBar from './components/AppBar'
+import {ThemeProvider} from '@material-ui/core/styles';
+import Theme from './utilities/Theme'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={Theme}>
+        <Router>
+          <AppBar/>
+          <Switch>
+            <Route exact path="/">
+              <div>Home</div>
+            </Route>
+            <Route path="/About/">
+              <div>About</div>
+            </Route>
+            <Route path="/ThemedWeek/">
+              <div>Themed Week</div>
+            </Route>
+            <Route path="/LatestPosts/">
+              <div>Latest Posts</div>
+            </Route>
+            <Route path="/ByMeal/">
+              <div>By Meal</div>
+            </Route>
+            <Route path="/ByType/">
+              <div>By Type</div>
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/utilities/Theme.js
@@ -0,0 +1,9 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+
+const RSTheme = createMuiTheme({
+  status: {
+    danger: 'red',
+  },
+});
+
+export default RSTheme
