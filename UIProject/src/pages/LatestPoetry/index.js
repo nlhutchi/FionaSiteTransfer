1afd211293e14792a794472c8344d896b6a19974
nlhutchi <30396281+nlhutchi@users.noreply.github.com>
2023-03-22 20:53:34 -0700

    Pool, Group, and Recipe Styling

--- UIProject/src/pages/LatestPoetry/index.js
+++ UIProject/src/pages/LatestPoetry/index.js
@@ -3,6 +3,7 @@ import { Grid, Typography, Box } from '@mui/material';
 import APIEndpoints from '../../data/APIEndpoints';
 import axios from 'axios';
 import useCKContext from '../../context/CKContext';
+import './index.css'
 
 
 function LatestPoetry(props) {
@@ -21,25 +22,30 @@ function LatestPoetry(props) {
     return (
         <>
             <Typography variant="h2" className='pageTitle'>Latest Poetry</Typography>
-            <Typography>I know, this has always been a food blog. Your one-stop-shop for healthy, easy recipes. And I promise it will continue to be! But here's the thing- food and poetry aren't so far apart to me. Both are there to nourish us: one for your body and one for your mind. And both come from my heart to yours— hopefully, to make your life a little bit easier: whether that's in figuring out what to do in the kitchen or in reading something that helps you figure out what's going on in your mind.</Typography>
+            <Typography className='introWrapper'>I know, this has always been a food blog. Your one-stop-shop for healthy, easy recipes. And I promise it will continue to be! But here's the thing- food and poetry aren't so far apart to me. Both are there to nourish us: one for your body and one for your mind. And both come from my heart to yours— hopefully, to make your life a little bit easier: whether that's in figuring out what to do in the kitchen or in reading something that helps you figure out what's going on in your mind.</Typography>
             <Grid>
                 {
                     poems.map((poem) => {
-                        return
+                        return <Grid container spacing={{ xs: 4 }} columns={{ xs: 1, md: 2 }}>
+                            {poems.map((poem, index) => (
+                                <Grid item xs={1} key={index} style={{ background: poem.styling.background }}>
+                                    {
+                                        poem.poemContent.map((section, i2) => {
+                                            return <div className='stanza' style={{ textAlign: section.styling.alignment }}>
+                                                {
+                                                    section.content.split('\n').map((poemLine) => {
+                                                        console.log(poemLine)
+                                                        return <p key={`p-${index}-${i2}`} variant='p'>{poemLine}</p>
+                                                    })
+                                                }
+                                            </div>
+                                        })
+                                    }
+                                </Grid>
+                            ))}
+                        </Grid>
                     })
                 }
-            </Grid>
-            
-            <Grid container spacing={{ xs: 4 }} columns={{ xs: 1, md: 2 }}>
-                {poems.map((poem, index) => (
-                    <Grid item xs={1} key={index}>
-                        {
-                            poem.poemContent.map((section, i2) => {
-                                return <Typography key={`p-${index}-${i2}`} variant='p'>{section.content}</Typography>
-                            })
-                        }
-                    </Grid>
-                ))}
             </Grid>
         </>
     );
